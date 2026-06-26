c6c639424e7ee3d7e11670b19382ed78d9e1cd17
Arthur Matheus Ferreira Hoffmann <arthur.hoffmann2010@gmail.com>
2026-06-26 09:26:51 -0300

    Update login.js

--- Monkichoc/js/login.js
+++ Monkichoc/js/login.js
@@ -9,6 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!nome || nome.trim() === "") {
             alert("Digite seu nome!");
             return;
+
+            if (!localStorage.getItem("usuario")) {
+                window.location.href = "login.html";
+            }
         }
 
         localStorage.setItem("usuario", nome);
